7f388c120d464a4518e03a6f4f8c8ded22d3dc75
adrianofpereira <nanojau@gmail.com>
2024-11-26 16:53:55 -0300

    Adicionar arquivos de projeto.

---
+++ Xadrez_Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xadrez_Console.Tabuleiro;
+
+namespace Xadrez_Console
+{
+    class program
+    {
+        static void Main(string[] Args)
+        {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posicao: " + P);
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
