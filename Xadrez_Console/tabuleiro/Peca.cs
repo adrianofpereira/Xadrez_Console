e959e37d375ca0b180181de3db4534a8f9fe8c4d
adrianofpereira <nanojau@gmail.com>
2024-11-27 15:38:18 -0300

    "Inserir Peças no Tabuleiro"

--- Xadrez_Console/tabuleiro/Peca.cs
+++ Xadrez_Console/tabuleiro/Peca.cs
@@ -14,9 +14,9 @@ namespace Xadrez_Console.tabuleiro
         public int qtdMovimentos { get; protected set; }
         public Tabuleiro tab {  get; protected set; }
 
-        public Peca(Posicao posicao, Tabuleiro tab, Cor cor)
+        public Peca(Tabuleiro tab, Cor cor)
         {
-            this.posicao = posicao;
+            this.posicao = null;
             this.tab = tab;
             this.cor = cor;
             this.qtdMovimentos = 0; 
