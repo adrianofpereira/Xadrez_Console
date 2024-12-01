86adcc4ab275d2d53abedc24d809ceb4fe38cbdc
adrianofpereira <nanojau@gmail.com>
2024-12-01 13:13:50 -0300

    "Melhorando a Impressão do Tabuleiro"

--- Xadrez_Console/Program.cs
+++ Xadrez_Console/Program.cs
@@ -10,28 +10,30 @@ namespace Xadrez_Console
     {
         static void Main(string[] Args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+            //PosicaoXadrez pos = new PosicaoXadrez('a', 1);
 
-            Console.WriteLine(pos);
+            //Console.WriteLine(pos);
 
-            Console.WriteLine(pos.toPosicao());
+            //Console.WriteLine(pos.toPosicao());
 
+            //Console.ReadLine();
+
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
-
-            //try
-            //{
-            //    Tabuleiro tab = new Tabuleiro(8, 8);
-
-            //    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            //    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            //    tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
-
-            //    Tela.imprimirTabuleiro(tab);
-            //}
-            //catch (TabuleiroException e){
-            //        Console.WriteLine(e.Message);
-            //    }
-            // Console.ReadLine();
         }
     }            
     
